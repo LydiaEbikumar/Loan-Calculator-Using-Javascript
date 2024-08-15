e5fd6bae8691b964af417d695bdded9049e74b6c
LydiaEbikumar <lydiaebikumar@gmail.com>
2024-08-15 21:50:36 +0530

    Add files via upload

---
+++ Loan Calculator/javascript.js
@@ -0,0 +1,7 @@
+function calculateEMI() {
+    var loanAmount = document.getElementById('loanAmount').value;
+    var interestRate = document.getElementById('interestRate').value / 1200; 
+    var loanTenure = document.getElementById('loanTenure').value * 12; 
+    var emi = (loanAmount * interestRate * Math.pow((1 + interestRate), loanTenure)) / (Math.pow((1 + interestRate), loanTenure) - 1);
+    document.getElementById('result').innerHTML = 'Your Monthly EMI: $' + emi.toFixed(2);
+}
